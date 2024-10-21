16863a5fc1c6a2fd495bc356865023cd037dbf1d
xrui <xie_rui1996@163.com>
2024-10-21 18:48:30 +0800

    Begin offline

--- src/utils/optimization.py
+++ src/utils/optimization.py
@@ -66,4 +66,74 @@ class Optimization(object):
         sol['theta_H'] = theta_H.X
 
         return sol
+    
+    @staticmethod
+    def opt_offline(param, data):
+        """
+        Optimize the offline problem
+        """
+        model = gp.Model('opt_offline')
+
+        ## Variables
+        a_F = model.addMVar((param['Num_F'], data['Num_T']), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        q_F = model.addMVar((param['Num_F'], data['Num_T'] + 1), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        m_R = model.addMVar((param['Num_F'], param['Num_B'], data['Num_T']), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        p_B = model.addMVar((param['Num_B'], data['Num_T']), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        q_B = model.addMVar((param['Num_B'], data['Num_T'] + 1), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        p_SC = model.addMVar((param['Num_B'], data['Num_T']), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        p_SD = model.addMVar((param['Num_B'], data['Num_T']), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        e_S = model.addMVar((param['Num_B'], data['Num_T'] + 1), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        p_C = model.addMVar((param['Num_B'], data['Num_T']), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+        tau_H = model.addMVar((param['Num_B'], data['Num_T'] + 1), lb=-float('inf'), vtype=GRB.CONTINUOUS)
+
+        ## Objective
+        obj = 0
+        for t in range(data['Num_T']):
+            obj = obj + sum(sum(param['gamma_R'] * m_R[:, :, t])) + param['gamma_F'] @ (data['A_F'][:, t] - a_F[:, t]) + param['gamma_S'] @ (p_SC[:, t] + p_SD[:, t]) + data['gamma_P'][:, t] @ (p_B[:, t] + p_SC[:, t] - p_SD[:, t] + p_C[:, t])
+        model.setObjective(obj, GRB.MINIMIZE)
+
+        ## Constraints
+        # Initialization
+        model.addConstr(q_F[:, 0] == 0)
+        model.addConstr(q_B[:, 0] == 0)
+        model.addConstr(e_S[:, 0] == param['E_Su'])
+        model.addConstr(tau_H[:, 0] == param['T_Hu'])
+        for t in range(data['Num_T']):
+            # 1a
+            model.addConstr(q_F[:, t + 1] == q_F[:, t] + a_F[:, t] - m_R[:, :, t] @ np.ones((param['Num_B'],)))
+            # 1b
+            model.addConstr(q_B[:, t + 1] == q_B[:, t] + m_R[:, :, t].T @ np.ones((param['Num_F'],)) - p_B[:, t])
+            # 1c
+            model.addConstr(a_F[:, t] >= 0)
+            model.addConstr(a_F[:, t] <= data['A_F'][:, t])
+            # 1d
+            for i in range(param['Num_F']):
+                model.addConstr(m_R[i, :, t] >= 0)
+                model.addConstr(m_R[i, :, t] <= param['M_Ro'][i, :])
+            # 1e
+            model.addConstr(p_B[:, t] >= 0)
+            model.addConstr(p_B[:, t] <= param['P_Bo'])
+            # 1f
+            model.addConstr(q_F[:, t] >= 0)
+            model.addConstr(q_F[:, t] <= param['Q_Fo'])
+            # 1g
+            model.addConstr(q_B[:, t] >= 0)
+            model.addConstr(q_B[:, t] <= param['Q_Bo'])
+        
+        # model.setParam('OutputFlag', 0)
+        model.optimize()
+
+        traj = {}
+        traj['a_F'] = a_F.X
+        traj['q_F'] = q_F.X
+        traj['m_R'] = m_R.X
+        traj['p_B'] = p_B.X
+        traj['q_B'] = q_B.X
+        traj['p_SC'] = p_SC.X
+        traj['p_SD'] = p_SD.X
+        traj['e_S'] = e_S.X
+        traj['p_C'] = p_C.X
+        traj['tau_H'] = tau_H.X
+
+        return traj
  
